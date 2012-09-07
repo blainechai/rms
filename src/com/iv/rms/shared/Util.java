da2e800ec2a9a132c8c3080454d7724e6c2bb940
Vasile Iacome <iv_eleven@yahoo.com>
2012-09-07 19:39:10 +0300

    Removed some files

--- src/com/iv/rms/shared/Util.java
+++ src/com/iv/rms/shared/Util.java
@@ -2,20 +2,24 @@ package com.iv.rms.shared;
 
 import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.TimeZone;
 
 public class Util {
 	
 	public static Integer formatDate(Date date){
 		SimpleDateFormat sdf = new SimpleDateFormat("yyyyMMdd");
+		sdf.setTimeZone(TimeZone.getTimeZone("GMT"));
 		String str = sdf.format(date);
 		return Integer.parseInt(str);
 	}
 	
 	public static Integer getMinutesSinceMidnight(Date date){
 		Integer minutes = 0 ;
-		SimpleDateFormat sdf = new SimpleDateFormat("kk");
+		SimpleDateFormat sdf = new SimpleDateFormat("HH");
+		sdf.setTimeZone(TimeZone.getTimeZone("GMT"));
 		minutes = Integer.parseInt(sdf.format(date)) * 60 ;
 		sdf = new SimpleDateFormat("mm");
+		sdf.setTimeZone(TimeZone.getTimeZone("GMT"));
 		minutes += Integer.parseInt(sdf.format(date));
 		return minutes;
 	}
