9455a15e5dad8e674d84adb985cc910688e8aafb
Vasile Iacome <iv_eleven@yahoo.com>
2012-09-12 22:21:36 +0300

    Chat test; Remove gwt-bootstrap

---
+++ src/com/iv/rms/chat/ChatService.java
@@ -0,0 +1,25 @@
+package com.iv.rms.chat;
+
+import com.google.appengine.api.xmpp.JID;
+import com.google.appengine.api.xmpp.Message;
+import com.google.appengine.api.xmpp.MessageBuilder;
+import com.google.appengine.api.xmpp.SendResponse;
+import com.google.appengine.api.xmpp.XMPPService;
+import com.google.appengine.api.xmpp.XMPPServiceFactory;
+
+public class ChatService {
+	
+	public void sendMessage(String id, String message){
+		JID jid = new JID(id);
+		Message msg = new MessageBuilder().withRecipientJids(jid).withBody(message).build();
+		boolean messageSent = false;
+		XMPPService xmpp = XMPPServiceFactory.getXMPPService();
+		SendResponse status = xmpp.sendMessage(msg);
+		messageSent = (status.getStatusMap().get(jid) == SendResponse.Status.SUCCESS);
+		if (!messageSent) {
+			// Send an email message instead...
+			System.out.println("Failed to send the message");
+		}
+	}
+
+}
