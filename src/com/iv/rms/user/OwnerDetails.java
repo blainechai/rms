869c528cc9a3c946e81bfe01df9d32af6f480cdd
Vasile Iacome <vasile@iacome.com>
2013-06-08 22:25:52 +0300

    Add twilio dependecy Create object OwnerDetails needed to store phone number

---
+++ src/com/iv/rms/user/OwnerDetails.java
@@ -0,0 +1,13 @@
+package com.iv.rms.user;
+
+import javax.jdo.annotations.Persistent;
+
+public class OwnerDetails {
+	
+	@Persistent
+	private String userId;
+
+	@Persistent
+	private String phone;
+
+}
